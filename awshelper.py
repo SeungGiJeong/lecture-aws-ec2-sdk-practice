d60570c62d25cecbdf51e5936dab8e09b32f007c
SeungGiJeong <jjsk0119@gmail.com>
2021-08-09 02:27:45 +0900

    update tag, key, userdata

--- awshelper.py
+++ awshelper.py
@@ -11,6 +11,25 @@ import botocore
 region = 'ap-northeast-2'    # AWS region
 session = boto3.Session(profile_name='lecture')
 
+
+def assemble_userdata():
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    combined_message = MIMEMultipart()
+
+    userdata_files = [
+        ('userdata.txt', 'text/x-shellscript')
+    ]
+
+    for fname, mimetype in userdata_files:
+        with open(fname, "r") as f:
+            content = f.read()
+        sub_message = MIMEText(content, mimetype, sys.getdefaultencoding())
+        sub_message.add_header('Content-Disposition', 'attachment; filename="{}"'.format(fname))
+        combined_message.attach(sub_message)
+    return combined_message
+
+
 def describe_instances():
     """
     Describes all EC2 instances associated with an AWS account
@@ -30,12 +49,27 @@ def run_instance():
     """
     ami_id        = "ami-04876f29fd3a5e8ba"   # AMI Id
     instance_type = "t2.micro"       # Instance Type
+    tag_specs     = [
+                        {
+                            'ResourceType': 'instance',
+                            'Tags': [
+                                {
+                                    'Key': 'Name',
+                                    'Value': 'BoB10@ProductDev.sdk.4126'
+                                }
+                            ]
+                        }
+                    ]
 
     # Instantiate the service resource object
     ec2_resource = session.resource('ec2', region_name=region)
     # Run an instance
+
     instances = ec2_resource.create_instances(ImageId=ami_id, InstanceType=instance_type,
-                                              MaxCount=1, MinCount=1)
+                                              MaxCount=1, MinCount=1, KeyName='BoB10@ProductDev.4126',
+                                              TagSpecifications=tag_specs, 
+                                              SecurityGroupIds=['sg-01aef88d0444fc7e9'],
+                                              UserData=assemble_userdata().as_string())
     Instance_id = instances[0].id
     print('\nInstance Id:      ' + Instance_id)
     print('Image Id:         ' + instances[0].image_id)
